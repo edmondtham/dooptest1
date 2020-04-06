55b58c7d2141ab08e3714cb8173e866e4fbb8d3b
edmond <edmondtham@hotmail.com>
2020-04-06 13:34:49 +0800

    Initial check-in of module CoreMVCTest

---
+++ CoreMVCTest/Data/MvcMovieContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CoreMVCTest.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreMVCTest.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
